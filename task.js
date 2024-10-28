6154dbf43ed95dcfc75bcba3057be3af4d95f954
{furytm} <kanuemma367@gmail.com>
2024-10-28 03:06:14 -0700

    second commit

--- task.js
+++ task.js
@@ -1,11 +1,11 @@
-function chunkArray(array, size) {
-    const result = [];
-    for (let i = 0; i < array.length; i += size) {
-        result.push(array.slice(i, i + size));
-    }
-    return result;
-}
 
-// Example usage:
-console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3)); // Output: [[1, 2, 3], [4, 5, 6], [7]]
-console.log(chunkArray([1, 2, 3, 4, 5], 2));      // Output: [[1, 2], [3, 4], [5]]
+const chunk = (arr, size) => 
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+      
+      arr.slice(i * size, i * size + size)
+    );
+  
+  
+  console.log(chunk([1, 2, 3, 4, 5, 6, 7], 3)); 
+  console.log(chunk([1, 2, 3, 4, 5], 2)); 
+
